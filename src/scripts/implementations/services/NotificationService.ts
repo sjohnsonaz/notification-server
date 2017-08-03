2b3f4c8fb536161c12bd2c117cd751cb42a48c92
Sean Johnson <sjohnson@sjohnsonaz.com>
2017-08-03 15:58:16 -0700

    Updated post return value.

--- src/scripts/implementations/services/NotificationService.ts
+++ src/scripts/implementations/services/NotificationService.ts
@@ -19,31 +19,44 @@ export default class SubscriptionService extends Controller<Express.Router, Expr
         this.deleteService = config.deleteService;
     }
 
+    deleteSubscription(subscription: ISubscription) {
+        return new Promise<any>((resolve, reject) => {
+            http.request({
+                method: 'DELETE',
+                host: this.deleteService.host,
+                port: this.deleteService.port,
+                path: this.deleteService.path + subscription.keys.auth
+            }, (res) => {
+                if (res.statusCode === 200) {
+                    resolve(res);
+                } else {
+                    reject(new Error('Status Code: ' + res.statusCode));
+                }
+            });
+        });
+    }
+
     @route('post', '/', false)
     async post(req, res) {
         let notification = JSON.stringify(req.body.notification);
         let subscriptions: ISubscription[] = req.body.subscriptions;
         try {
-            await Promise.all(subscriptions.map(async (subscription) => {
+            let results = await Promise.all(subscriptions.map(async (subscription) => {
                 try {
                     await Push.triggerPushMsg(subscription, notification);
+                    return true;
                 } catch (err) {
                     if (err.statusCode === 410) {
-                        http.request({
-                            method: 'DELETE',
-                            host: this.deleteService.host,
-                            port: this.deleteService.port,
-                            path: this.deleteService.path + subscription.keys.auth
-                        }, (res) => {
-
-                        });
+                        // Not waiting for delete
+                        this.deleteSubscription(subscription);
                     } else {
                         console.log('Subscription is no longer valid: ', err);
                     }
+                    return err;
                 }
             }));
             res.setHeader('Content-Type', 'application/json');
-            res.send(JSON.stringify({ data: { success: true } }));
+            res.send(JSON.stringify({ results: results }));
         } catch (err) {
             res.status(500);
             res.setHeader('Content-Type', 'application/json');
